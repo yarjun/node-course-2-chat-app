94b8e0613fdffdc8fb23a690e13db7d84d8ab256
Codinghunterr <arjunyadav15597@gmail.com>
2017-09-19 02:23:45 +0530

    add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,34 @@
+var socket = io();
+
+// socket.on('connect',function(){
+//   console.log('connect to server');
+//
+//   socket.emit('createEmail',{
+//     to: 'jen@email.com',
+//     text: 'hey . this is arjun'
+//   });
+// });
+// socket.on('disconnect',function(){
+//   console.log('Disconnect from server');
+// });
+//
+// socket.on('newEmail',function(email){
+//   console.log('new email',email);
+// });
+
+socket.on('connect',function(){
+  console.log('connected to server');
+
+  socket.emit('createMessage',{
+    from: 'abc@example.com',
+    text: 'client to server'
+  });
+});
+
+socket.on('newMessage',function(message){
+  console.log('newMessage',message);
+});
+
+socket.on('disconnect',function(){
+  console.log('disconnect from server');
+});
--- server/server.js
+++ server/server.js
@@ -4,8 +4,8 @@ var app = express();
 const http = require('http');
 const socketIO = require('socket.io');
 
-const port = process.env.PORT || 3000;
 const publicpath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 
 var server = http.createServer(app);
@@ -13,12 +13,41 @@ var io = socketIO(server);
 
 app.use(express.static(publicpath));
 
-io.on('connection',function(socket){
-  console.log('New user connected');
+// io.on('connection',function(socket){
+//   console.log('New user connected');
+//
+//   socket.emit('newEmail',{
+//     from: 'arjun@example.com',
+//     text: 'hey how are you',
+//     createdAt: 123
+//   });
+//
+//   socket.on('createEmail',function(email){
+//     console.log('create email',email);
+//   })
+//
+//   socket.on('disconnect',function(){
+//     console.log('user disconnected');
+//   });
+// });
 
-  socket.on('disconnect',function(){
-    console.log('user disconnected');
+
+
+io.on('connection',function(socket){
+  console.log('new user connected');
+
+  socket.emit('newMessage',{
+    from: 'arjun@example.com',
+    text: 'server to client',
+    createdAt: 123
   });
+socket.on('disconnect',function(){
+  console.log('user disconnected');
+});
+
+socket.on('createMessage',function(message){
+  console.log('createMessage',message);
+});
 
 });
 

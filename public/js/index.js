e8958d9051c206677ea20c1af8742375c5f48196
Codinghunterr <arjunyadav15597@gmail.com>
2017-09-21 23:06:54 +0530

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -60,11 +60,12 @@ socket.on('disconnect',function(){
 jQuery('#message-form').on('submit',function(e){
   e.preventDefault();
 
+  var messageTextbox = jQuery('[name=message]');
   socket.emit('createMessage',{
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextbox.val()
   },function(){
-
+      messageTextbox.val('');
   });
 });
 
@@ -75,13 +76,16 @@ locationButton.on('click',function(){
     return alert('Geolocation not sported by your browser');
   }
 
+  locationButton.attr('disabled','disabled').text('Sending location...');
+
   navigator.geolocation.getCurrentPosition(function(position){
-    console.log(position);
+    locationButton.removeAttr('disabled').text('Send location');
     socket.emit('createLocationMessage',{
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
     });
   },function(){
+    locationButton.removeAttr('disabled').text('Send location');
     alert('Unable to fetch location');
   });
 
